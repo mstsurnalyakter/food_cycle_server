ff5a9baf89e6f2bdedca6311c6957c54de9fb84c
mstsurnalyakter <surnalyms@gmail.com>
2024-11-07 09:33:47 +0600

    add: quiz collection

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const  app = express();
+const app = express();
 
 //middleware
-app.use(cors());
+//building middleware
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-
-
+// app.use(cors());
+// app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,23 +40,50 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-     const database = client.db("FoodCycleDB");
-     const categoriesCollection = database.collection("categories");
-     const foodWiseCollection = database.collection("food_wise");
+    const database = client.db("FoodCycleDB");
+    const categoriesCollection = database.collection("categories");
+    const foodWiseCollection = database.collection("food_wise");
+    const childContentsCollection = database.collection("child_contents");
+    const quizCollection = database.collection("quiz")
 
-     app.get("/categories",async(req,res)=>{
-        const result = await categoriesCollection.find().toArray();
-        res.send(result);
+    app.get("/categories", async (req, res) => {
+      const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    });
 
-     })
-     app.get("/foodWise", async (req, res) => {
-       const result = await foodWiseCollection.find().toArray();
-       res.send(result);
-     });
-     app.get("/food_wise/:id", async (req, res) => {
-       const result = await foodWiseCollection.findOne({_id:new ObjectId(req.params.id)});
-       res.send(result);
-     });
+    app.get("/foodWise", async (req, res) => {
+      const result = await foodWiseCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/food_wise/:id", async (req, res) => {
+      const result = await foodWiseCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    app.get("/child_contents", async (req, res) => {
+      const result = await childContentsCollection.find().limit(5).toArray();
+      res.send(result);
+    });
+
+    app.get("/child_contents/:id", async (req, res) => {
+      const result = await childContentsCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    app.get("/quiz/:commonId", async (req, res) => {
+      const query = {
+        commonId: req.params.commonId,
+      };
+      console.log(req.params.commonId);
+      const result = await quizCollection.find(query).toArray();
+      res.send(result);
+    });
+
 
 
     console.log(
@@ -60,12 +96,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Food Cycle Server");
+});
 
-
-app.get("/",(req,res)=>{
-    res.send("Welcome to Food Cycle Server");
-})
-
-app.listen(port,()=>{
-    console.log(`Food Cycle Server is running at : http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Food Cycle Server is running at : http://localhost:${port}`);
+});
